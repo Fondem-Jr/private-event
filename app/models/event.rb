19414a6a59dc90bd766e00155e1a42d82612c9b2
Fondem Junior <fondemjunior@yahoo.com>
2021-06-22 00:41:06 +0100

    Create and migrate all necessary tables and foreign keys

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
     belongs_to :creator, class_name: 'User'
+    has_many :attendances, foreign_key: 'attended_event_id'
+    has_many :attendees, class_name: 'User', through: 'attendances'
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :events, foreign_key: 'creator_id'
+  has_many :attendances, foreign_key: 'attendee_id'
+  has_many :attended_events, class_name: 'Event', through: :attendances
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,

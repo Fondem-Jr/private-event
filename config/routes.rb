9e0a8b953112360460554fc86d9d1da31b78e3a4
Fondem Junior <fondemjunior@yahoo.com>
2021-06-22 00:25:32 +0100

    Add the association between the event attendee and the event

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :events
-  resources :users
+  resources :attendances
   #get 'event/index'
   root 'events#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_205154) do
+ActiveRecord::Schema.define(version: 2021_06_21_232313) do
+
+  create_table "attendances", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "attended_event_id"
+    t.integer "attendee_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "title"

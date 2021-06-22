b3495390125becb0964280a08837ac584a23ee69
Fondem Junior <fondemjunior@yahoo.com>
2021-06-22 01:13:51 +0100

    Create and migrate all necessary tables and foreign keys

---
+++ app/controllers/attendances_controller.rb
@@ -0,0 +1,18 @@
+class AttendancesController < ApplicationController
+    def index
+        @attendances = Attendance.all
+    end
+    def create
+        @event = current_user.attended_events.find_by_id(params[:attended_event_id])
+        if @event.nil?
+          @attendance = current_user.attendances.build(attended_event_id: params[:attended_event_id])
+          if @attendance.save
+            redirect_to root_path
+          else
+            redirect_to current_user
+          end
+        else
+          redirect_to root_path
+        end
+      end
+end

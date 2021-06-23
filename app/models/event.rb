328b5c8d2cbe1102ce71fe479676cbcf65cd944e
DanteAlonsoHT <dante1818@hotmail.com>
2021-06-23 12:17:58 -0500

    Add relations among the models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,8 @@
 class Event < ApplicationRecord
-    belongs_to :creator, class_name: 'User'
-    has_many :attendances, foreign_key: 'attended_event_id'
-    has_many :attendees, class_name: 'User', through: 'attendances'
-
-    scope :upcoming_events, -> { where('date > ?', Date.today) }
+  belongs_to :user, foreign_key: :user_id, class_name: 'User'
+  has_and_belongs_to_many :attendance, join_table: 'attendances', class_name: 'Attendance'
+  
+  scope :upcoming_events, -> { where('date > ?', Date.today) }
   scope :previous_events, -> { where('date < ?', Date.today) }
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-  has_many :events, foreign_key: 'creator_id'
-  has_many :attendances, foreign_key: 'attendee_id'
-  has_many :attended_events, class_name: 'Event', through: :attendances
+  has_many :events, foreign_key: :user_id
+  has_and_belongs_to_many :attendances, join_table: 'events', class_name: 'Event'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,

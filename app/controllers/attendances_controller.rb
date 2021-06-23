1f1d0fbb9fc6ae0201ccc20746eda1a110b0149a
DanteAlonsoHT <dante1818@hotmail.com>
2021-06-23 11:23:07 -0500

    Update methods in attendaces_controller

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,18 +1,25 @@
 class AttendancesController < ApplicationController
     def index
-        @attendances = Attendance.new
+      @attendances = Attendance.all
     end
+
     def create
-        @event = current_user.attended_events.find_by_id(params[:attended_event_id])
-        if @event.nil?
-          @attendance = current_user.attendances.build(attended_event_id: params[:attended_event_id])
-          if @attendance.save
-            redirect_to root_path
-          else
-            redirect_to current_user
-          end
+      @attendance = Attendance.new(attendance_params)
+
+      respond_to do |format|
+        if @attendance.save
+          format.html { redirect_to root_path, notice: "Attendance was successfully created." }
+          format.json { render :index, status: :created, location: @attendance }
         else
-          redirect_to root_path
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @attendance.errors, status: :unprocessable_entity }
         end
       end
+    end
+
+    private
+
+    def attendance_params
+      params.permit(:event_id, :user_id)
+    end
 end

e0ceb03ebf7496bfe2ebd2631169fcee6c605129
Fondem Junior <fondemjunior@yahoo.com>
2021-06-24 18:53:56 +0100

    Give the list of the attended events and upcoming events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    @events = Event.where(user_id:current_user.id)
+    @events = Event.where
+    (user_id:current_user.id)
     @past_events = @events.previous_events
     @upcoming_events = @events.upcoming_events
   end
@@ -12,4 +13,5 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
   end
+  
 end

964eedf50b86baa0c7906cf569117db76c93ecea
DanteAlonsoHT <dante1818@hotmail.com>
2021-06-24 10:13:12 -0500

    Create show method in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @events = Event.all.where(user_id: current_user.id)
+  end
+
   def create
     @user = User.new(params[:user])
   end

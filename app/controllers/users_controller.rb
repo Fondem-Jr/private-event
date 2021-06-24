f12ffb589ebec0ed607f149a80e9d1dce46c6f64
Fondem Junior <fondemjunior@yahoo.com>
2021-06-24 19:01:08 +0100

    fix linters errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @events = Event.where(user_id:current_user.id)
+    @events = Event.where(user_id: current_user.id)
     @past_events = @events.previous_events
     @upcoming_events = @events.upcoming_events
   end
@@ -12,5 +12,4 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
   end
-
 end

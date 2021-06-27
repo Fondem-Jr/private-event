46054ceb4b00c2a8cfd0d557ae5cf662abc6a2c4
Fondem Junior <fondemjunior@yahoo.com>
2021-06-27 14:22:47 +0100

    Add the routes for user#show

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :attendances
   #get 'event/index'
   root 'events#index'
-  get 'past-upcoming-events', action: :show, controller: 'users'
+  get 'user/show', action: :show, controller: 'users'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
